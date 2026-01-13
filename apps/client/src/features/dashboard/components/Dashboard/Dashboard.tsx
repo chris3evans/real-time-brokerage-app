b4d5a085f619d9f7629467e8b595af7ba6829706
chris3evans <chris3evans.ce@gmail.com>
2026-01-13 18:47:21 +0000

    [Fix] - moved balance amount and balance return setter functions inside a useEffect hook

--- apps/client/src/features/dashboard/components/Dashboard/Dashboard.tsx
+++ apps/client/src/features/dashboard/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,14 @@
 import { DashboardBalance } from '@dashboard-balance/DashboardBalance';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Dashboard = () => {
   const [balanceAmount, setBalanceAmount] = useState<number>(0);
   const [balanceReturn, setBalanceReturn] = useState<number>(0);
+
+  useEffect(() => {
+    setBalanceAmount(10000);
+    setBalanceReturn(712);
+  }, [])
 
   return (
     <>
