4228efcfb930640f5901affe10b682c62aede0c6
chris3evans <chris3evans.ce@gmail.com>
2026-01-13 17:40:01 +0000

    [Feature] - application primary, secondary, tertiary colours as well as basic elements for DashboardBalance component

--- apps/client/src/features/dashboard/components/DashboardBalance/DashboardBalance.tsx
+++ apps/client/src/features/dashboard/components/DashboardBalance/DashboardBalance.tsx
@@ -2,12 +2,16 @@ import styles from './DashboardBalance.module.scss';
 
 export const DashboardBalance = () => {
   return (
-    <div className={styles['test']}>Dashboard Balance Component</div>
-    // Welcome Message (?)
-    // Title
-    // Current Balance
-    // Profit/Loss
-    // Deposit Button
-    // Withdraw Button
+    <div className={styles['dashboard-balance']}>
+      <h3>Account Balance</h3>
+      <div className={styles['balance']}>
+        <span className={styles['balance-amount']}>£5,365</span>
+        <span className={styles['balance-return']}>+£356 (7.12%)</span>
+      </div>
+      <div className={styles['balance-buttons']}>
+        <button className={styles['cta-button']}>Deposit</button>
+        <button className={styles['cta-button']}>Withdraw</button>
+      </div>
+    </div>
   )
 }
