2c55c3d870dbb2f15d947b0bae7cc55a81a162ed
chris3evans <chris3evans.ce@gmail.com>
2026-01-15 18:08:48 +0000

    [Feature] - planned out new DashboardPortfolioPerformance component

--- apps/client/src/features/dashboard/components/DashboardPortfolioPerformance/DashboardPortfolioPerformance.tsx
+++ apps/client/src/features/dashboard/components/DashboardPortfolioPerformance/DashboardPortfolioPerformance.tsx
@@ -1,3 +1,7 @@
 export const DashboardPortfolioPerformance = () => {
+  // Top Bar
+  // // Section Title + Interval Period Selector
+
+  // Performance Chart
   return <div>Dashboard Portfolio Performance Component</div>;
 };
