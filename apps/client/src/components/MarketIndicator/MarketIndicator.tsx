6893cb87ad539ea211b640d00f8b6692b56e376b
chris3evans <chris3evans.ce@gmail.com>
2026-01-15 17:28:29 +0000

    [Feature] - simplified MarketIndicator design

--- apps/client/src/components/MarketIndicator/MarketIndicator.tsx
+++ apps/client/src/components/MarketIndicator/MarketIndicator.tsx
@@ -3,11 +3,7 @@ import styles from "./MarketIndicator.module.scss";
 export const MarketIndicator = () => {
   return (
     <div className={styles["indicator"]}>
-      <div className={styles["indicator-heading"]}>
-        {/* <img src="" className={styles["indicator-symbol"]} /> */}
-        *image*
-        <span className={styles["indicator-name"]}>FTSE 100</span>
-      </div>
+      <h5>FTSE 100</h5>
       <div className={styles["indicator-value"]}>
         <span className={styles["indicator-price"]}>7,125.50 </span>
         <span className={styles["indicator-change"]}>-25.30 (-0.35%)</span>
