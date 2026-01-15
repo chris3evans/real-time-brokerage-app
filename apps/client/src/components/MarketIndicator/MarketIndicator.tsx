c457b39072b582ed77a567fd0d5f712e4ec27da3
chris3evans <chris3evans.ce@gmail.com>
2026-01-15 17:59:46 +0000

    [Feature] - changes in market indicators now have red and green styling applied

--- apps/client/src/components/MarketIndicator/MarketIndicator.tsx
+++ apps/client/src/components/MarketIndicator/MarketIndicator.tsx
@@ -6,12 +6,23 @@ export const MarketIndicator = ({
   price,
   change,
 }: MarketIndicatorProps) => {
+  const percentageChange = ((change / (price - change)) * 100).toFixed(2);
+  let changeStatus = "";
+  if (change > 0) {
+    changeStatus = styles["positive"];
+  }
+  if (change < 0) {
+    changeStatus = styles["negative"];
+  }
+
   return (
     <div className={styles["indicator"]}>
       <h5>{name}</h5>
       <div className={styles["indicator-value"]}>
         <span className={styles["indicator-price"]}>{price} </span>
-        <span className={styles["indicator-change"]}>{change} (-0.35%)</span>
+        <span className={`${styles["indicator-change"]} ${changeStatus}`}>
+          {change} ({percentageChange})
+        </span>
       </div>
     </div>
   );
