fdc83dc8a9d767a8cd56b30919fa61647b1e2d60
chris3evans <chris3evans.ce@gmail.com>
2026-01-16 17:41:45 +0000

    [Feature] - markup for performance duration selection

--- apps/client/src/features/dashboard/components/DashboardPortfolioPerformance/DashboardPortfolioPerformance.tsx
+++ apps/client/src/features/dashboard/components/DashboardPortfolioPerformance/DashboardPortfolioPerformance.tsx
@@ -11,12 +11,21 @@ import {
 } from "recharts";
 import { AppColour } from "@/types/app.enums";
 import { PORTFOLIO_PERFORMANCE_MOCKDATA } from "@/mock-data/portfolio-performance.mockdata";
+import { Button } from "@/components/Button/Button";
 
 export const DashboardPortfolioPerformance = () => {
   return (
     <Card className={styles["portfolio-performance"]}>
       <div className={styles["portfolio-performance-heading"]}>
-        Portfolio Performance Bar will go here
+        <h4>Portfolio Performance</h4>
+        <div className={styles["duration-selector"]}>
+          <Button label="1D" />
+          <Button label="1W" style="outline" />
+          <Button label="1M" style="outline" />
+          <Button label="6M" style="outline" />
+          <Button label="1Y" style="outline" />
+          <Button label="5Y" style="outline" />
+        </div>
       </div>
       <ResponsiveContainer width="100%" height={300}>
         <AreaChart
