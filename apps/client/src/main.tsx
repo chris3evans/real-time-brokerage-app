d2980a0f592c5b291f8314dff5227eb0e0252202
chris3evans <chris3evans.ce@gmail.com>
2026-01-20 18:49:07 +0000

    [Feature] - created and implemented QueryClientProvider wrapper

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,11 +4,25 @@ import "./index.scss";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000,
+      refetchOnWindowFocus: true,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
