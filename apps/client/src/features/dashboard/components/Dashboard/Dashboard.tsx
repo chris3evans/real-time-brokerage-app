162bd7015fd904f5216c7cbbfacc7ab5c665c1c7
chris3evans <chris3evans.ce@gmail.com>
2026-01-22 18:43:59 +0000

    [Feature] - market indicator list now renders with mock data from mock api

--- apps/client/src/features/dashboard/components/Dashboard/Dashboard.tsx
+++ apps/client/src/features/dashboard/components/Dashboard/Dashboard.tsx
@@ -4,23 +4,8 @@ import { DashboardMarketOverview } from "@/features/dashboard/components/Dashboa
 import { DashboardPortfolioPerformance } from "@dashboard-portfolio-performance/DashboardPortfolioPerformance";
 import { DashboardWatchList } from "@dashboard-watchlist/DashboardWatchList";
 import { DashboardPortfolioOverview } from "@dashboard-portfolio-overview/DashboardPortfolioOverview";
-import { useMarketIndicators } from "@/hooks/dashboard.hooks";
 
 export const Dashboard = () => {
-  const { data, isLoading, isError, error } = useMarketIndicators();
-
-  if (isLoading) return <div>DATA IS LOADING PLEASE WAIT!!!!</div>;
-
-  if (isError) {
-    return (
-      <div>
-        Error: {error instanceof Error ? error.message : "Unknown error"}
-      </div>
-    );
-  }
-
-  console.log(data, "MOCK DATA");
-
   return (
     <div className={styles["dashboard"]}>
       <DashboardBalance />
