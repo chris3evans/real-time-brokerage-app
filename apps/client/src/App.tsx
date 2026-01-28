c2408872470a1b7bc72d232e9cae2cc52095f8a5
chris3evans <chris3evans.ce@gmail.com>
2026-01-28 17:05:26 +0000

    [Feature] - added dependencies for Radix package

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,24 +1,10 @@
-// import { useEffect, useState } from "react";
 import { Dashboard } from "@dashboard/Dashboard";
 
 // const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 function App() {
-  // const [message, setMessage] = useState("Loading...");
-
-  // useEffect(() => {
-  //   fetch(API_URL + "/profile")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       setMessage(data.username);
-  //     })
-  //     .catch(() => setMessage("API is not responding"));
-  // }, []);
-
   return (
     <div className="container">
-      {/* <p>API Status: <strong>{message}</strong></p> */}
       <Dashboard />
     </div>
   );
