9868e13b41b1e51973ac77516f2ccc85f96b90df
chris3evans <chris3evans.ce@gmail.com>
2026-01-28 20:51:52 +0000

    [Feature] - navigation hides and reveals when menu button clicked

--- apps/client/src/features/navigation/components/SideNavigation/SideNavigation.tsx
+++ apps/client/src/features/navigation/components/SideNavigation/SideNavigation.tsx
@@ -4,11 +4,14 @@ import { IconButton } from "@/components/IconButton/IconButton";
 import { Menu } from "lucide-react";
 import { AppColour } from "@/types/app.enums";
 import { useState } from "react";
-import { useAppDispatch } from "@/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { setSideNavigationOpen } from "@/store/navigation/sideNavigationSlice";
 
 export const SideNavigation = () => {
   const dispatch = useAppDispatch();
+  const sideNavigationOpen = useAppSelector(
+    (state) => state.navigation.sideNavigationOpen,
+  );
 
   const [showNavigation, setShowNavigation] = useState<boolean>(false);
 
@@ -23,16 +26,40 @@ export const SideNavigation = () => {
         <Menu color={AppColour.WHITE} />
       </IconButton>
       <div className={styles["navigation-items"]}>
-        <div className={clsx(styles["navigation-item"], "actionable")}>
+        <div
+          className={clsx(
+            styles["navigation-item"],
+            "actionable",
+            sideNavigationOpen ? "" : styles["hide"],
+          )}
+        >
           Dashboard
         </div>
-        <div className={clsx(styles["navigation-item"], "actionable")}>
+        <div
+          className={clsx(
+            styles["navigation-item"],
+            "actionable",
+            sideNavigationOpen ? "" : styles["hide"],
+          )}
+        >
           Search
         </div>
-        <div className={clsx(styles["navigation-item"], "actionable")}>
+        <div
+          className={clsx(
+            styles["navigation-item"],
+            "actionable",
+            sideNavigationOpen ? "" : styles["hide"],
+          )}
+        >
           Portfolio
         </div>
-        <div className={clsx(styles["navigation-item"], "actionable")}>
+        <div
+          className={clsx(
+            styles["navigation-item"],
+            "actionable",
+            sideNavigationOpen ? "" : styles["hide"],
+          )}
+        >
           Settings
         </div>
       </div>
