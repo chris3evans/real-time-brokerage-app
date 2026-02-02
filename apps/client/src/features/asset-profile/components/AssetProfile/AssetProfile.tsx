7a1bed8e06abb67d0a9a9d5de192fd4cf67019d3
chris3evans <chris3evans.ce@gmail.com>
2026-02-02 21:08:39 +0000

    [Feature] - planned out asset profile contents and layout order

--- apps/client/src/features/asset-profile/components/AssetProfile/AssetProfile.tsx
+++ apps/client/src/features/asset-profile/components/AssetProfile/AssetProfile.tsx
@@ -4,5 +4,13 @@ import { useParams } from "react-router-dom";
 export const AssetProfile = () => {
   const { name } = useParams<{ name: string }>();
 
-  return <View>Asset Profile will be rendered here for {name}</View>;
+  return (
+    <View>
+      Asset Profile will be rendered here for {name}
+      {/* Header - name, ticker, current price + price change */}
+      {/* Price history graph */}
+      {/* LEFT (2/3) - Asset details */}
+      {/* RIGHT (1/3) - Buy / Sell asset buttons */}
+    </View>
+  );
 };
