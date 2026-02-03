c1a0b8183feadc7cf2477be4e4cfe370b43b8195
chris3evans <chris3evans.ce@gmail.com>
2026-02-03 21:50:37 +0000

    [Feature] - details markup and basic styling

--- apps/client/src/features/asset-profile/components/AssetDetails/AssetDetail.tsx
+++ apps/client/src/features/asset-profile/components/AssetDetails/AssetDetail.tsx
@@ -1,5 +1,65 @@
 import styles from "./AssetDetails.module.scss";
 
 export const AssetDetails = () => {
-  return <div className={styles["asset-details"]}>Asset Details</div>;
+  return (
+    <div className={styles["asset-details"]}>
+      <div className={styles["asset-overview"]}>
+        <h5>Company Overview</h5>
+        <p className="thin">
+          This is where the long paragraph containing the company overview will
+          go
+        </p>
+      </div>
+      <div className={styles["details"]}>
+        <div className={styles["detail"]}>
+          <span>Market Cap:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Trading Volume:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>PE Ratio:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Dividend Yield:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Dividend Amount:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Revenue Per Share:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Net Income Per Share:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Earnings Per Share:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Revenue Growth:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>EPS Growth:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Gross Profit Margin:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+        <div className={styles["detail"]}>
+          <span>Net Profit Margin:</span>
+          <span className="thin">1,000,000,000</span>
+        </div>
+      </div>
+    </div>
+  );
 };
