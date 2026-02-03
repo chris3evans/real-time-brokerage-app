c4eb6338d6b3732aaa5d8b2b7a2e51f37fdfbe26
chris3evans <chris3evans.ce@gmail.com>
2026-02-03 18:01:22 +0000

    [Feature] - styling and markup for asset profile header

--- apps/client/src/features/asset-profile/components/AssetProfile/AssetProfile.tsx
+++ apps/client/src/features/asset-profile/components/AssetProfile/AssetProfile.tsx
@@ -1,13 +1,19 @@
 import { View } from "@/components/View/View";
 import { useParams } from "react-router-dom";
+import { AssetProfileHeader } from "../AssetProfileHeader/AssetProfileHeader";
 
 export const AssetProfile = () => {
   const { name } = useParams<{ name: string }>();
 
   return (
     <View>
-      Asset Profile will be rendered here for {name}
-      {/* Header - name, ticker, current price + price change */}
+      <AssetProfileHeader
+        assetName={name ?? ""}
+        assetTicker="TCKR"
+        currentPrice={100}
+        change={1}
+        changePercentage={1}
+      />
       {/* Price history graph */}
       {/* LEFT (2/3) - Asset details */}
       {/* RIGHT (1/3) - Buy / Sell asset buttons */}
---
+++ apps/client/src/features/asset-profile/components/AssetProfileHeader/AssetProfileHeader.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { AssetProfileComponentProps } from "../../types/asset-profile.interfaces";
+import styles from "./AssetProfileHeader.module.scss";
+import { formatChangeColour } from "@/utlities/utilities.service";
+
+export const AssetProfileHeader = ({
+  assetName,
+  assetTicker,
+  currentPrice,
+  change,
+  changePercentage,
+}: AssetProfileComponentProps) => {
+  return (
+    <div className={styles["header"]}>
+      <h4>{assetName}</h4>
+      <span>{assetTicker}</span>
+      <h1>${currentPrice}</h1>
+      <div className={clsx(styles["change"], formatChangeColour(change))}>
+        <span>
+          {change > 0 ? "+" : ""}
+          {change}
+        </span>
+        <span>({changePercentage}%)</span>
+      </div>
+    </div>
+  );
+};
