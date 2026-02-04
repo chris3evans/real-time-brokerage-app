06b09c897933a1ec4d52ef53c23dade952442024
chris3evans <chris3evans.ce@gmail.com>
2026-02-04 18:28:25 +0000

    [Feature] - base mock data for stock details

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -17,6 +17,22 @@ export interface StockPositionItem extends StockItem {
   positionValue?: number;
 }
 
+export interface AssetDetails {
+  overview: string;
+  marketCap: number;
+  tradingVolume: number;
+  peRatio: number;
+  dividendYield: number;
+  dividendAmount: number;
+  revenuePerShare: number;
+  netIncomePerShare: number;
+  earningsPerShare: number;
+  revenueGrowth: number;
+  epsGrowth: number;
+  grossProfitMargin: number;
+  netProfitMargin: number;
+}
+
 export interface LineGraphPoint {
   time: string;
   value: number;
