68ad06e61e0627b2d2985054994981cc875b3928
chris3evans <chris3evans.ce@gmail.com>
2026-02-04 18:51:10 +0000

    [Feature] - raw component files and grid placement within asset profile view

---
+++ apps/client/src/features/asset-profile/components/AssetProfileActions/AssetProfileActions.tsx
@@ -0,0 +1,7 @@
+import styles from "./AssetProfileActions.module.scss";
+
+export const AssetProfileActions = () => {
+  return (
+    <div className={styles["asset-actions"]}>This is the actions section</div>
+  );
+};
