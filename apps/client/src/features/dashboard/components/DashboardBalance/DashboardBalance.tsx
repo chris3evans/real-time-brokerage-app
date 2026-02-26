47f5dda961266d1ecae4e80d3dc34eb106231522
chris3evans <chris3evans.ce@gmail.com>
2026-02-26 21:02:45 +0000

    [Feature] - use app balance state in balance

--- apps/client/src/features/dashboard/components/DashboardBalance/DashboardBalance.tsx
+++ apps/client/src/features/dashboard/components/DashboardBalance/DashboardBalance.tsx
@@ -5,14 +5,11 @@ import { ButtonSet } from "@/components/ButtonSet/ButtonSet";
 import { useAppSelector } from "@/store/hooks";
 import { useState } from "react";
 import { GeneralModal } from "@/components/modals/GeneralModal/GeneralModal";
+import { Input } from "@/components/Input/Input";
+import { Button } from "@/components/Button/Button";
 
 export const DashboardBalance = () => {
-  const balanceAmount = useAppSelector(
-    (state) => state.dashboard.balanceAmount,
-  );
-  const balanceReturn = useAppSelector(
-    (state) => state.dashboard.balanceReturn,
-  );
+  const balanceAmount = useAppSelector((state) => state.user.balance);
 
   const [openModal, setOpenModal] = useState<boolean>(false);
   const [modalTitle, setModalTitle] = useState<string>("");
@@ -33,8 +30,8 @@ export const DashboardBalance = () => {
     <Card cardTitle="Account Balance" className={styles["dashboard-balance"]}>
       <div className={styles["balance"]}>
         <span className={styles["balance-amount"]}>£{balanceAmount}</span>
-        <span className={formatChangeColour(balanceReturn)}>
-          {`${balanceReturn > 0 ? " +" : ""}${balanceReturn}`} (7.12%)
+        <span className={formatChangeColour(10)}>
+          {`${10 > 0 ? " +" : ""}${10}`} (7.12%)
         </span>
       </div>
       <ButtonSet
@@ -52,7 +49,8 @@ export const DashboardBalance = () => {
         closeModal={closeModal}
         title={modalTitle}
       >
-        <div>I AM THE MODALS CONTENT</div>
+        <Input type="number" />
+        <Button label="Confirm" />
       </GeneralModal>
     </Card>
   );
